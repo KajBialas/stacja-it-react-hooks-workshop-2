b413a999108612a683a0d83a669573a33e5bab39
Kaj Białas <kaj.bialas@dev-industry.com>
2020-08-23 10:16:11 +0200

    isse 3 final

--- src/WindowResize.js
+++ src/WindowResize.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect} from 'react';
 
 function WindowResize() {
   const [windowHeight, setHeight] = useState(0);
+  const [windowWidth, setWidth] = useState(0);
 
   useEffect(() => {
+    setHeight(window.innerHeight);
+    setWidth(window.innerWidth);
     window.addEventListener('resize', handleWindowResize);
 
     return () => {
@@ -17,11 +20,12 @@ function WindowResize() {
 
   const handleWindowResize = () => {
     setHeight(window.innerHeight);
+    setWidth(window.innerWidth);
   };
 
   return(
     <div>
-      {windowHeight}
+      {windowHeight} x {windowWidth}
     </div>
   )
 }

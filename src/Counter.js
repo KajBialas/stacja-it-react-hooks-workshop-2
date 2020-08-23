7876c7503775e1672bdaf075c7cb87f5f52ede88
Kaj Białas <kaj.bialas@dev-industry.com>
2020-08-23 09:45:44 +0200

    Final issue2

--- src/Counter.js
+++ src/Counter.js
@@ -4,11 +4,15 @@ function Counter() {
   const [counterValue, setCounterValue] = useState(10);
   const handleIncrementCounter = () => setCounterValue(counterValue + 1);
   const handleSafeIncrementCounter = () => setCounterValue(prevCounter => prevCounter + 1);
+  const handleSafeDecrementCounter = () => setCounterValue(prevCounter => prevCounter - 1);
+  const handleResetCounter = () => setCounterValue(0);
 
   return (
     <div>
       Counter: {counterValue}
       <button onClick={handleSafeIncrementCounter}>+</button>
+      <button onClick={handleSafeDecrementCounter}>-</button>
+      <button onClick={handleResetCounter}>RESET</button>
     </div>
   )
 }

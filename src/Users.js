efd60efda2694224be76984758e76eee9faf1570
Kaj Białas <kaj.bialas@dev-industry.com>
2020-08-23 11:13:31 +0200

    better solution issue4

--- src/Users.js
+++ src/Users.js
@@ -4,17 +4,29 @@ const API_URL = 'https://jsonplaceholder.typicode.com/users';
 
 function Users() {
   const [ usersList, setUsersList ] = useState([]);
+  const [ usersListLoading, setUsersListLoading ] = useState(false);
+  const [ usersListError, setUsersListError ] = useState(false);
 
   useEffect(() => {
+    setUsersListLoading(true);
     fetch(API_URL)
       .then(response => response.json())
-      .then((response) => setUsersList(response));
+      .then((response) =>
+        setTimeout(
+          () => {
+            setUsersListLoading(false);
+            setUsersList(response)},
+          3000)
+      )
+      .catch((e) => setUsersListError(true));
   }, []);
 
   const renderUsersList = () => usersList.map(userItem => <div key={userItem.id}>{userItem.name}</div>);
 
   return (
     <div>
+      {usersListLoading ? <div>Ładowanie...</div> : null}
+      {usersListError ? <div>Błąd ładowania danych</div> : null}
       {renderUsersList()}
     </div>
   )
